ba23cfd1af5b41279ab7fbd09c1475f2f5e3f5bd
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-20 17:53:32 -0400

    commit tree 745dde8fc1029c3d536c7f83ca5d7b81fee0c09b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    let twoDrivers = [...arrayOfDrivers]
+    return twoDrivers.splice(0,2)
+}
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+    let twoDrivers = [...arrayOfDrivers]
+    return twoDrivers.splice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(integer) {
+
+    return function(integer){integer * 4}
+    
+}
+
+function fareDoubler(params) {
+    const multiplyer = createFareMultiplier(integer)
+}
